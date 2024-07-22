be5048d74afb1cb62c4f2778e0a1f88653a0c7a7
Dominik Šelmeci <dominik.selmeci@gmail.com>
2024-07-22 22:03:03 +0200

    feat: add UI tiles and simple drag&drop system

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("ON DROP" + (transform.childCount == 0).ToString());
+        if (transform.childCount == 0)
+        {
+            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
+            inventoryItem.parentAfterDrag = transform;
+        }
+    }
+}
